7e86c95348c67c2558d49b7f1dae9087ff8c9269
JanBur96 <jan.burghardt1706@icloud.com>
2020-08-29 14:15:17 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+
+    extend: {
+      colors: {
+        customPink: '#FFC7C7',
+        customPastel: '#FFE2E2',
+        customWhite: '#F6F6F6',
+        customGrey: '#8785A2'
+      },
+      screens: {
+        'xs': '600px'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
